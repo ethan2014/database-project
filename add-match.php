3703c32dff6578efdde80d99a5bf2aed3cac194c
Ethan Lape <lapeed@miamioh.edu>
2016-05-03 23:14:50 -0400

    final commit

--- add-match.php
+++ add-match.php
@@ -3,25 +3,22 @@
     <head>
 	<meta charset="utf-8">
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
-	<meta content="width=device-width, initial-scale=1" name="viewport"><!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<title>
 	    Rocket Stats - Match
-	</title><!-- Bootstrap -->
+	</title>
 	<link href="css/site.css"  rel="stylesheet" >
-	<link href="css/bootstrap.min.css" rel="stylesheet"><!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<script src="js/jquery.js"></script>
     </head>
     <body>
 	<nav class="navbar navbar-default">
 	    <div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-		    <button aria-expanded="false" class="navbar-toggle collapsed" data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" type="button">
+		    <button aria-expanded="false"
+			    class="navbar-toggle collapsed"
+			    data-target="#bs-example-navbar-collapse-1"
+			    data-toggle="collapse" type="button">
 			<span class="sr-only">Toggle navigation</span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
